fd19457d82ae918868367423114852cfbecccd01
tiktokyi <tiktokyi@gmail.com>
2025-01-22 00:32:43 +0900

    doc : alb 재설정

--- src/main/java/com/ahimmoyak/lms/config/CustomHealthIndicator.java
+++ src/main/java/com/ahimmoyak/lms/config/CustomHealthIndicator.java
@@ -1,4 +1,26 @@
 package com.ahimmoyak.lms.config;
 
-public class CustomHealthIndicator {
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        boolean isHealthy = checkCustomServiceHealth();
+        if (isHealthy) {
+            return Health.up().build();
+        } else {
+            return Health.down().withDetail("Custom Service", "Not available")
+                    .withDetail("Reason", "Service not reachable")
+                    .build();
+        }
+    }
+
+    private boolean checkCustomServiceHealth() {
+        return true;
+    }
+
 }
