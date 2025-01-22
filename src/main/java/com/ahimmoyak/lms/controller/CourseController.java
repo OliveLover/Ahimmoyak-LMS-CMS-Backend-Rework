5e0f012a7a3f04e44bb9960208d86d7acd0f1e7f
tiktokyi <tiktokyi@gmail.com>
2025-01-22 18:58:32 +0900

    mod : cors origin 추가

--- src/main/java/com/ahimmoyak/lms/controller/CourseController.java
+++ src/main/java/com/ahimmoyak/lms/controller/CourseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = {"http://localhost:5173", "https://www.ahimmoyak.shop"})
+@CrossOrigin(origins = {"http://localhost:5173", "https://www.ahimmoyak.shop", "https://ahimmoyak.shop"})
 public class CourseController {
 
     private final CourseService courseService;
