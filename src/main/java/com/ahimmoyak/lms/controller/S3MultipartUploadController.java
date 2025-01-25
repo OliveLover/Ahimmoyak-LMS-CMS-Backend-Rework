50ef888f647109f45405786d3a22b567f84a7966
tiktokyi <tiktokyi@gmail.com>
2025-01-25 17:42:05 +0900

    mod : HTTP 메서드 변경

--- src/main/java/com/ahimmoyak/lms/controller/S3MultipartUploadController.java
+++ src/main/java/com/ahimmoyak/lms/controller/S3MultipartUploadController.java
@@ -6,10 +6,7 @@ import com.ahimmoyak.lms.dto.upload.InitiateMultipartUploadResponseDto;
 import com.ahimmoyak.lms.service.S3MultipartUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import software.amazon.awssdk.services.s3.model.CompletedPart;
 
 import java.util.List;
@@ -25,12 +22,12 @@ public class S3MultipartUploadController {
         return s3MultipartUploadService.initiateMultipartUpload(requestDto);
     }
 
-    @PostMapping("/api/v1/s3/complete")
+    @PutMapping("/api/v1/s3/complete")
     public ResponseEntity<MessageResponseDto> completeMultipartUpload(@RequestParam String fileName, @RequestParam String uploadId, @RequestBody List<CompletedPart> completedParts) {
         return s3MultipartUploadService.completeMultipartUpload(fileName, uploadId, completedParts);
     }
 
-    @PostMapping("/api/v1/s3/abort")
+    @DeleteMapping("/api/v1/s3/abort")
     public ResponseEntity<MessageResponseDto> abortMultipartUpload(@RequestParam String fileName, @RequestParam String uploadId) {
         return s3MultipartUploadService.abortMultipartUpload(fileName, uploadId);
     }
