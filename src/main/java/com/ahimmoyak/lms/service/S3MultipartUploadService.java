07ceff3db8983924fa7ce04969242f4b8b400328
tiktokyi <tiktokyi@gmail.com>
2025-02-12 20:04:14 +0900

    fix : 컨텐츠 업로드시 다른 특정 컨텐츠가 사라지는 현상 수정, dynamoDB를 조회하는 과정에서 queryRequest에 sk를 추가하여 unique한 값을 명확하게 지정하여 해결

--- src/main/java/com/ahimmoyak/lms/service/S3MultipartUploadService.java
+++ src/main/java/com/ahimmoyak/lms/service/S3MultipartUploadService.java
@@ -183,8 +183,12 @@ public class S3MultipartUploadService {
         int videoDuration = requestDto.getVideoDuration();
         String filePath = generateS3FileUrl(requestDto.getFileKey());
 
+        log.info("contentId : {}", contentId);
+        log.info("fileKey : {}", fileKey);
+
         QueryEnhancedRequest queryRequest = QueryEnhancedRequest.builder()
-                .queryConditional(QueryConditional.keyEqualTo(k -> k.partitionValue(courseId)))
+                .queryConditional(QueryConditional.keyEqualTo(k -> k.partitionValue(courseId)
+                        .sortValue(contentId)))
                 .build();
 
         SdkIterable<Page<Content>> contentResult = contentsTable.query(queryRequest);
@@ -197,10 +201,12 @@ public class S3MultipartUploadService {
 
         if (content != null) {
             existingFileKey = content.getFileKey();
+            log.info("existingFileKey : {}", existingFileKey);
         }
 
         if (existingFileKey != null) {
             deleteFileFromS3(existingFileKey);
+            log.info("delete 완료!");
         }
 
         Content existingContent = contentsTable.getItem(r -> r.key(k -> k
